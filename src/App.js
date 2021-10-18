35d6ae93ea7711117a6b4ee6a3545949a3ffa9f8
marikben <70907667+marikben@users.noreply.github.com>
2021-10-18 20:39:19 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './App.css';
+import Header from './basework/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <main>
+        <p>
+          Moi!
+          </p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/basework/Header.js
@@ -0,0 +1,7 @@
+//import modulestyles from './css/modulestyles';
+
+export default function Header(){
+    return(
+    <div>
+        Hello
+    </div>);}
